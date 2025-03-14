7a9a735acfb920c813c6aa6eba7d524c558707d3
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-03-14 18:44:22 +0200

    o mica problema

--- components/Header.js
+++ components/Header.js
@@ -12,14 +12,16 @@ const Logo = styled(Link)`
 export default function Header() {
 	return (
 		<StyledHeader>
-			<Logo href={'/'}>Ecomerciantul</Logo>
-			<nav>
-				<Link href={'/'}>Home</Link>
-				<Link href={'/products'}>All products</Link>
-				<Link href={'/categories'}>Categories</Link>
-				<Link href={'/account'}>Account</Link>
-				<Link href={'/cart'}>Cart (0)</Link>
-			</nav>
+			<Center>
+				<Logo href={'/'}>Ecomerciantul</Logo>
+				<nav>
+					<Link href={'/'}>Home</Link>
+					<Link href={'/products'}>All products</Link>
+					<Link href={'/categories'}>Categories</Link>
+					<Link href={'/account'}>Account</Link>
+					<Link href={'/cart'}>Cart (0)</Link>
+				</nav>
+			</Center>
 		</StyledHeader>
 	);
 }
