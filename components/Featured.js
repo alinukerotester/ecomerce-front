6a06bda3434fdae6b01d04758a322f83c4d0a912
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-03-19 14:30:46 +0200

    urmeaza sa facem conexiunile

--- components/Featured.js
+++ components/Featured.js
@@ -10,14 +10,15 @@ const Bg = styled.div`
 const Title = styled.h1`
 	margin: 0;
 	font-weight: normal;
+	font-size: 3rem;
 `;
 const Desc = styled.p`
 	color: #aaa;
 	font-size: 0.8rem;
 `;
-const Wrapper = styled.div`
+const ColumnsWrapper = styled.div`
 	display: grid;
-	grid-template-columns: 0.8fr 1.2fr;
+	grid-template-columns: 0.9fr 1.1fr;
 	gap: 40px;
 	img {
 		max-width: 100%;
@@ -27,18 +28,36 @@ const Column = styled.div`
 	display: flex;
 	align-items: center;
 `;
+const ButtonsWrapper = styled.div`
+	display: flex;
+	gap: 10px;
+	margin-top: 25px;
+`;
 
 export default function Featured() {
 	return (
 		<Bg>
 			<Center>
-				<Wrapper>
+				<ColumnsWrapper>
 					<Column>
 						<div>
 							<Title>Pro anywhere</Title>
 							<Desc>Get the pro experience with our new Pro plan</Desc>
-							<button>Read more</button>
-							<Button size='l'>Add to cart</Button>
+							<ButtonsWrapper>
+								<Button outline white>
+									Read more
+								</Button>
+								<Button primary>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										viewBox='0 0 24 24'
+										fill='currentColor'
+										className='size-6'>
+										<path d='M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z' />
+									</svg>
+									Add to cart
+								</Button>
+							</ButtonsWrapper>
 						</div>
 					</Column>
 					<Column>
@@ -47,7 +66,7 @@ export default function Featured() {
 							alt='Imagine S3'
 						/>
 					</Column>
-				</Wrapper>
+				</ColumnsWrapper>
 			</Center>
 		</Bg>
 	);
