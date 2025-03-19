7830d54b8a0512d4981b6a6f46f10a5acafe5e8c
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-03-19 11:16:49 +0200

    fila noua

--- components/Header.js
+++ components/Header.js
@@ -9,19 +9,34 @@ const Logo = styled(Link)`
 	color: #fff;
 	text-decoration: none;
 `;
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: 20px 0;
+`;
+const StyledNav = styled.nav`
+	display: flex;
+	gap: 15px;
+`;
+const NavLink = styled(Link)`
+	color: #aaa;
+	text-decoration: none;
+`;
 
 export default function Header() {
 	return (
 		<StyledHeader>
 			<Center>
-				<Logo href={'/'}>Ecomerciantul</Logo>
-				<nav>
-					<Link href={'/'}>Home</Link>
-					<Link href={'/products'}>All products</Link>
-					<Link href={'/categories'}>Categories</Link>
-					<Link href={'/account'}>Account</Link>
-					<Link href={'/cart'}>Cart (0)</Link>
-				</nav>
+				<Wrapper>
+					<Logo href={'/'}>Ecomerciantul</Logo>
+					<StyledNav>
+						<NavLink href={'/'}>Home</NavLink>
+						<NavLink href={'/products'}>All products</NavLink>
+						<NavLink href={'/categories'}>Categories</NavLink>
+						<NavLink href={'/account'}>Account</NavLink>
+						<NavLink href={'/cart'}>Cart (0)</NavLink>
+					</StyledNav>
+				</Wrapper>
 			</Center>
 		</StyledHeader>
 	);
