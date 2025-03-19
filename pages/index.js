3a1d2aab8f557def1e27d62a44cee8f7e647c0f7
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-03-19 14:53:55 +0200

    urmeaza fisierul env

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Featured from '@/components/Featured';
 import Header from '@/components/Header';
+import { Product } from '@/models/Product';
 
-export default function HomePage() {
+export default function HomePage({ product }) {
 	return (
 		<div>
 			<Header />
 			<Featured />
 		</div>
 	);
+}
+
+export async function getServersideProps() {
+	const featuredProductId = '67613e79a0c0e31167663878';
+	await mongooseConnect();
+	const product = await Product.findById(featuredProductId);
+	return {
+		props: {
+			product,
+		},
+	};
 }
