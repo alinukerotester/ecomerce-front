6ffc4dc7d419e842018729b0944a7f17e1ceaf66
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-05-05 12:18:40 +0300

    o noua fila

---
+++ components/CartContext.js
@@ -0,0 +1,14 @@
+import { createContext, useState } from 'react';
+
+const CartContext = createContext({});
+
+export function CartContextProvider({ children }) {
+	const [cartProducts, setCartProducts] = useState([]);
+	return (
+		<CartContext.Provider value={{ cartProducts }}>
+			{children}
+		</CartContext.Provider>
+	);
+}
+
+export default CartContext;
