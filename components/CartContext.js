a3b4f3335818bc28faefad6c6fa2dff4e8d69dda
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-05-05 12:38:18 +0300

    ne pregatim de incheiere

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,14 +1,15 @@
 import { createContext, useState } from 'react';
 
-const CartContext = createContext({});
+export const CartContext = createContext({});
 
 export function CartContextProvider({ children }) {
 	const [cartProducts, setCartProducts] = useState([]);
+	function addProduct(productId) {
+		setCartProducts((prev) => [...prev, productId]);
+	}
 	return (
-		<CartContext.Provider value={{ cartProducts }}>
+		<CartContext.Provider value={{ cartProducts, setCartProducts, addProduct }}>
 			{children}
 		</CartContext.Provider>
 	);
 }
-
-export default CartContext;
