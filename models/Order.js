72c03e2083c6b6f5c2b7626fc1db34c9a6e9c550
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-06-17 16:44:43 +0300

    o alta fila

---
+++ models/Order.js
@@ -0,0 +1,12 @@
+const { Schema } = require('mongoose');
+
+const OrderSchema = new Schema({
+	line_items: Object,
+	name: String,
+	email: String,
+	city: String,
+	postalCode: String,
+	streetAddress: String,
+	country: String,
+	paid: Boolean,
+});
--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,7 +1,35 @@
-export default function handler(req, res) {
+import { mongooseConnect } from '@/lib/mongoose';
+import { Product } from '@/models/Product';
+
+export default async function handler(req, res) {
 	if (req.method === 'POST') {
-		res.json('Checkout successful');
+		res.json('should be a POST request');
 		return;
 	}
-	const { name, email, city, postalCode, streetAddress, country } = req.body;
+	const { name, email, city, postalCode, streetAddress, country, products } =
+		req.body;
+	await mongooseConnect();
+	const productsIds = products.split(',');
+	const uniqueIds = [...new Set(productsIds)];
+	const productsInfos = await Product.find({ _id: uniqueIds });
+
+	let line_items = [];
+	for (const productId of uniqueIds) {
+		const productInfo = productsInfos.find(
+			(p) => p._id.toString() === productId,
+		);
+		const quantity = productsIds.filter((id) => id === productId)?.length || 0;
+		if (quantity > 0 && productInfo) {
+			line_items.push({
+				quantity,
+				price_data: {
+					currency: 'usd',
+					product_data: { name: productInfo.title },
+					unit_amount: quantity * productInfo.price,
+				},
+			});
+		}
+	}
+
+	res.json({ line_items });
 }
