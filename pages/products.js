05881d4cf46f5420932c11533a367677cd9c0d34
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-06-23 11:08:30 +0300

    cam asa arata

--- pages/products.js
+++ pages/products.js
@@ -1,3 +1,29 @@
+import Center from '@/components/Center';
+import Header from '@/components/Header';
+import { mongooseConnect } from '@/lib/mongoose';
+import { Product } from '@/models/Product';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+	font-size: 1.5em;
+`;
+
 export default function ProductsPage() {
-	return;
+	return (
+		<>
+			<Header />
+			<Center>
+				<Title>All products</Title>
+			</Center>
+		</>
+	);
+}
+
+export async function getServerSideProps() {
+	await mongooseConnect();
+	return {
+		props: {
+			products: Product,
+		},
+	};
 }
