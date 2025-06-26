f4231d223c7de35b9770d9a0eb496f65a601aca3
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-06-26 15:25:41 +0300

    gata pentru azi

--- components/icons/Bars.js
+++ components/icons/Bars.js
@@ -1,17 +1,17 @@
 export default function BarsIcon({ className = 'w-6 h-6' }) {
 	return (
 		<svg
-			width='24'
-			height='24'
+			xmlns='http://www.w3.org/2000/svg'
 			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth={1.5}
 			stroke='currentColor'
-			strokeWidth='2'
-			strokeLinecap='round'
-			strokeLinejoin='round'
-			viewBox='0 0 24 24'>
-			<line x1='3' y1='12' x2='21' y2='12' />
-			<line x1='3' y1='6' x2='21' y2='6' />
-			<line x1='3' y1='18' x2='21' y2='18' />
+			className={className}>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+			/>
 		</svg>
 	);
 }
