3ba948605a2804b06f2d0b2887add8eca7e7814c
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-06-26 14:38:00 +0300

    cam gata site ul este functional

--- components/ProductImages.js
+++ components/ProductImages.js
@@ -17,6 +17,12 @@ const ImageButtons = styled.div`
 `;
 const ImageButton = styled.div`
 	border: 2px solid #ccc;
+	${(props) =>
+		props.active
+			? `
+        border-color: #ccc;`
+			: `
+            border-color: transparent;`}
 	height: 40px;
 	padding: 2px;
 	cursor: pointer;
