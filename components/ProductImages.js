e005b588a1f552ab26c523f178cbd320addee377
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-06-26 13:53:29 +0300

    acum e funtionala si pagina fiecarui produs

--- components/ProductImages.js
+++ components/ProductImages.js
@@ -1,7 +1,48 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Image = styled.img`
+	max-width: 100%;
+	max-height: 100%;
+`;
+const BigImage = styled.img`
+	max-width: 100%;
+	max-height: 200px;
+`;
+const ImageButtons = styled.div`
+	display: flex;
+	gap: 10px;
+	flex-grow: 0;
+	margin-top: 10px;
+`;
+const ImageButton = styled.div`
+	border: 2px solid #ccc;
+	height: 40px;
+	padding: 2px;
+	cursor: pointer;
+	border-radius: 5px;
+`;
+const BigImageWrapper = styled.div`
+	text-align: center;
+`;
+
 export default function ProductImages({ images, title }) {
+	const [activeImage, setActiveImage] = useState(images?.[0]);
 	return (
 		<>
-			<img src={images?.[0]} alt={title} />
+			<BigImageWrapper>
+				<BigImage src={activeImage} alt={title} />
+			</BigImageWrapper>
+			<ImageButtons>
+				{images.map((image) => (
+					<ImageButton
+						key={image}
+						active={image === activeImage}
+						onClick={() => setActiveImage(image)}>
+						<Image src={image} alt='' />
+					</ImageButton>
+				))}
+			</ImageButtons>
 		</>
 	);
 }
