12efff3a3516d7b05a11acd831e78fbf5e1a5ab9
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-07-04 12:22:48 +0300

    start test

--- components/Featured.js
+++ components/Featured.js
@@ -22,10 +22,13 @@ const Desc = styled.p`
 `;
 const ColumnsWrapper = styled.div`
 	display: grid;
-	grid-template-columns: 1.1fr 0.9fr;
+	grid-template-columns: 1fr;
 	gap: 40px;
 	img {
 		max-width: 100%;
+	}
+	@media screen and (min-width: 768px) {
+		grid-template-columns: 1.1fr 0.9fr;
 	}
 `;
 const Column = styled.div`
